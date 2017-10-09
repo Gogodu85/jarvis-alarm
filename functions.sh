af6cf1ed459f5d4615e80b5cbc5a2573009310a2
Pineapple-Geek <naim.jebbour@outlook.fr>
2017-10-09 18:51:37 +0200

    Mise à Jour 1.1 / Update 1.1

--- functions.sh
+++ functions.sh
@@ -11,7 +11,7 @@ pg_alarm_main_fr () {
 say "$(pg_jarvis-alarm_fr "alarm_hours" "$1" "$2")" 
 
 # convert wakeup time to seconds
-target_h=$(($1 * 60 * 60))
+target_h=$(($1 * 3600))
 target_m=$(($2 * 60))
 target_s_t=$(($target_h + $target_m))
 
@@ -33,10 +33,10 @@ time_m=$(date +%M)
 
 if [ $wake_music != "null" ]
 then
-   mpg321 $wake_music
+   play $wake_music
 fi
 
-say "Réveillez vous il es $time_h:$time_m"
+say "Réveillez-vous il es $time_h heures $time_m"
 }
 
 # ---------------------------------------------------------------------------------------------------------
