21f6c6a4d6e1dfb98ddf65c8241e1fe6ecd1f2ff
Pineapple-Geek <naim.jebbour@outlook.fr>
2018-01-16 13:53:08 +0100

    Mise à Jour 1.4 / Update 1.4
    Volume Progressif

--- command.sh
+++ command.sh
@@ -1,1 +1,14 @@
-mpg321 plugins_installed/jarvis-alarm-test/audio/melodie.mp3
+#------------------------------------------
+#-- Volume Progressif Par PineApple-Geek --
+#------------------------------------------
+volume=10 #Volume Minimal souhaiter
+MAX_VOLUME=100 #Volume Maximal souhaiter
+amixer -c 1 set Master ${volume}% #Demarrage du volume au minimal 
+mpg321 melodie.mp3 & #Lancement de la musique  
+while [ "$volume" -lt "$MAX_VOLUME" ] ; do 
+   amixer -c 1 set Master ${volume}% 
+   echo "$volume"
+   ((volume=$volume+5)) 
+   killall qiv #Commande pour empêcher les fenêtres de s'empiler  
+   sleep 1 # décider de combien de temps le processus va s'arreter entre les augmentations de volume
+done 
